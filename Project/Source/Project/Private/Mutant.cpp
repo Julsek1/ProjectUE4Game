33d7308befe135e2609bdee3557a8c07ed8b0a18
Julian Grajales <juliangrajales@gmail.com>
2020-06-28 18:15:38 -0400

    Stealth Kill
    Implemented widget to show Stealth Kill hint when the player is close enough to the enemy to perform the fatal attack when "K" key is pressed.
    Added Death animMontage.
    Update Behavior Tree to check if the character is dead before entering any branch available.

--- Project/Source/Project/Private/Mutant.cpp
+++ Project/Source/Project/Private/Mutant.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Mutant.h"
+#include "JBasePlayer.h"
+#include "Animation/AnimInstance.h"
 
 // Sets default values
 AMutant::AMutant()
@@ -11,6 +13,9 @@ AMutant::AMutant()
 
 	Hp = 90.f;
 	MaxHp = 100.f;
+	Damage = 20.f;
+
+	IsDeseased = false;
 
 }
 
@@ -33,5 +38,43 @@ void AMutant::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+}
+
+float AMutant::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+
+	if (Hp - DamageAmount <= 0.f)
+	{
+		//Hp -= DamageAmount;
+		Hp = 0.f;
+		Death(DamageCauser);
+	}
+	else
+	{
+		Hp -= DamageAmount;
+	}
+	return DamageAmount;
+}
+
+void AMutant::Death(AActor* DamageMaker)
+{
+	UAnimInstance* AnimationInst = GetMesh()->GetAnimInstance();
+	if (AnimationInst)
+	{
+		AnimationInst->Montage_Play(FightMontage, 1.0f);
+		AnimationInst->Montage_JumpToSection(FName("Death"), FightMontage);
+	}
+	
+	//SetFEnemyMovStatus(EFEnemyMoveStat::FEMS_Death);
+	//GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	
+
+	//IsFighting = false;
+
+	AJBasePlayer* Player = Cast<AJBasePlayer>(DamageMaker);
+	if (Player)
+	{
+		Player->FightGoalUpdate();
+	}
 }
 
