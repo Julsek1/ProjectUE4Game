8a3e4816e8f6291db19f89cd79364e9aad9969f3
Philip Frattolillo <phil.fratt@gmail.com>
2020-07-29 12:17:48 -0400

    Main menu hover color/Mutant prep
    Changed the main menu hover color from red to orange.
    Created a mutant class.
    Created a blueprint from that class and configured it.
    Created an animation blueprint and blendspace for the mutant.
    Created an AI controller, blackboard and behavior tree for the mutant.
    Created a task that determines if the mutant is in melee range and damages the player.
    Configured the blendspace with some animations and configured it to work with the animation blueprint.

---
+++ Project/Source/Project/Private/TSMutant.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TSMutant.h"
+
+ATSMutant::ATSMutant()
+{
+	EnemyName = "Mutant";
+}
---
+++ Project/Source/Project/Public/TSMutant.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ParentEnemy.h"
+#include "TSMutant.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_API ATSMutant : public AParentEnemy
+{
+	GENERATED_BODY()
+
+public:
+	ATSMutant();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		float MeleeDamage = 0.15f;
+};
